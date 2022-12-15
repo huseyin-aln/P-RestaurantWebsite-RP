6ee4d3d83f0a0032de5adf0c4c47e3a36774ab57
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-15 14:22:54 +0300

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import BannerImage from "../assets/2blackbean.jpg";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Contact Us</h1>
+        <form id="contact-form">
+          <label htmlFor="name">Full Name</label>
+          <input type="text" name="name" placeholder="Enter your name..." />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Enter your email..." />
+          <label htmlFor="message">Message</label>
+          <textarea
+            type="text"
+            name="name"
+            placeholder="Enter yor message..."
+          />
+
+          <button type="submit"> Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
