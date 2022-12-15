7ec16656eae03bc005023c19b99612c43d52a33f
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-15 12:45:34 +0300

    home and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="socialMedia">
+        <p>Copyright &copy; 2022, {"<h-aln>"}</p>
+        <div className="icons">
+          <FaInstagram />
+          <FaTwitter />
+          <FaFacebook />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
