864f93ba8d564c42492db2c54d4eccc3dae0196f
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-15 11:32:05 +0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Home from "../pages/Home";
+import Menu from "../pages/Menu";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
